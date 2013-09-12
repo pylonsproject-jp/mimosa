dc420de7e8c554b56b03287e0c824350be9d010c
Atsushi Odagiri <aodagx@gmail.com>
2013-09-12 22:34:36 +0900

    start up

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import os
+
+__version__ = '0.0'
+__author__ = 'pylonsproject.jp'
+
+requires = [
+    "pyramid",
+    "pyramid_tm",
+    "pyramid_layout",
+    "sqlalchemy",
+    "zope.sqlalchemy",
+    "deform",
+    "colander",
+    "deform_bootstrap",
+]
+
+tests_require = [
+]
+
+here = os.path.dirname(__file__)
+
+def _read(name):
+    try:
+        with open(name) as f:
+            return f.read()
+    except IOError:
+        return ""
+
+readme = _read(os.path.join(here, 'README.rst'))
+changes = _read(os.path.join(here, 'CHANGES.txt'))
+contrib = _read(os.path.join(here, 'CONTRIBUTORS.txt'))
+
+setup(name="mimosa",
+      version=__version__,
+      author=__author__,
+      description="administrator interface for sqlalchemy with pyramid.",
+      long_description=readme+"\n"+changes+"\n"+"contrib",
+      packages=find_packages(exclude=['tests', 'demo']),
+      install_requires=requires,
+      tests_require=tests_require,
+      extras_require={
+          "testing": tests_require,
+      },
+)
